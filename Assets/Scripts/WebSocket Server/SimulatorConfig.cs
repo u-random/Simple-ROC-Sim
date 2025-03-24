00a3f9b5362f8392306920004e4622614d1be759
u-random <isaklars@outlook.com>
2025-03-24 08:52:59 +0100

    Fixed telemetry position bug

--- Assets/Scripts/WebSocket Server/SimulatorConfig.cs
+++ Assets/Scripts/WebSocket Server/SimulatorConfig.cs
@@ -3,7 +3,7 @@ public static class SimulatorConfig
 {
     // Map Configuration
     public static readonly double[] MapCenterCoordinates = new double[] { 10.570455, 59.425565 }; // {Longitude, Latitude}
-    public static readonly float UnityUnitsToKm = 0.1f;
+    public static readonly float UnityUnitsToKm = 0.001f; // One unity unit = 1 meter
     public static readonly float MapSizeKm = 12f;
     // Networking Configuration
     public static readonly float TelemetryUpdateRate = 0.1f;
